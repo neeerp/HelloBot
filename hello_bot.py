fa11b6f9eb4e389a0a75a81578f3a1219b132b47
neeerp <davey_wavey@rocketmail.com>
2017-11-19 17:25:57 -0500

    Tidied up and added personalized greetings.

--- hello_bot.py
+++ hello_bot.py
@@ -1,43 +1,90 @@
 import praw
-import pdb
 import re
 import os
 
 
-if __name__ == "__main__":
+class HelloBot:
+    """
+        A reddit bot that scans the 5 hottest submissions in a subreddit and
+        replies to all posts containing the string 'hello world' with
+        'Hello there!'.
+    """
+    def __init__(self):
+        """
+            Initialize Hello bot, loading in its list of posts to ignore and
+            preparing an instance of praw.Reddit
+        """
+        self._reddit = praw.Reddit('hello_bot')
 
-    reddit = praw.Reddit("hello_bot")
+        # Initialize or load a list containing the posts Hellobot should ignore
+        if not os.path.isfile('posts_replied_to.txt'):
+            self.posts_replied_to = []
+        else:
+            with open('posts_replied_to.txt', 'r') as f:
+                post_string = f.read()
+                self._posts_replied_to = post_string.split("\n")
+                self._posts_replied_to = \
+                    list(filter(None, self._posts_replied_to))
 
-    # Create/load a list of posts not to reply to in the future
-    if not os.path.isfile("posts_replied_to.txt"):
-        posts_replied_to = []
-    else:
-        with open("posts_replied_to.txt", "r") as f:
-            posts_replied_to = f.read()
-            posts_replied_to = posts_replied_to.split("\n")
-            posts_replied_to = list(filter(None, posts_replied_to))
+    def _update_replied_to(self):
+        """ Update the file list of posts hellobot has replied to """
+        with open('posts_replied_to.txt', 'w') as f:
+            for post_id in self._posts_replied_to:
+                f.write(post_id + '\n')
 
-    # For the hottest 5 posts in r/neeerp
-    subreddit = reddit.subreddit('neeerp')
-    for submission in subreddit.hot(limit=5):
+    def _reply_to_post(self, post):
+        """
+            First, check to see if the post is a comment or submission, then
+            reply if its text contains 'hello world' and hellobot hasn't
+            already replied!
 
-        # First, see if we should reply to the submission
-        if submission.id not in posts_replied_to:
-            if re.search("hello world", submission.selftext, re.IGNORECASE):
-                submission.reply("Hello there!")
-                posts_replied_to.append(submission.id)
+            :param post: The post that hellobot will parse and maybe reply to
+            :type post: praw.Submission || praw.Comment
+        """
+        if isinstance(post, praw.models.Submission):
+            post_text = post.selftext
+        else:
+            post_text = post.body
 
-        # Iterate through and reply to every comment that has "hello world"
+        if post.id not in self._posts_replied_to and \
+                re.search('hello world', post_text, re.IGNORECASE):
+
+            post.reply('Hello there, /u/%s!' % post.author.name)
+            self._posts_replied_to.append(post.id)
+
+    def _check_comment_tree(self, submission):
+        """
+            Iterate over a submission's comment tree, replying to each
+            applicable comment.
+
+            :param submission: The comment tree to iterate over
+            :type submission: praw.Submission
+        """
+        # Get rid of 'uncollapsed' comments
         submission.comments.replace_more(limit=0)
+
         comment_queue = submission.comments[:]
         while comment_queue:
             comment = comment_queue.pop(0)
             comment_queue.extend(comment.replies)
 
-            if comment.id not in posts_replied_to:
-                if re.search("hello world", comment.body, re.IGNORECASE):
-                    comment.reply("Hello there!")
-                    posts_replied_to.append(comment.id)
-    with open("posts_replied_to.txt", "w") as f:
-        for post_id in posts_replied_to:
-            f.write(post_id + "\n")
+            self._reply_to_post(comment)
+
+    def say_hello(self, subreddit):
+        """
+            Iterate through the 5 hottest submissions in a given subreddit
+            and have hellobot say hello to any posts (comments OR submissions)
+            containing the string 'hello world'.
+
+            :param subreddit: The subreddit to check
+            :type subreddit: str
+        """
+        for submission in self._reddit.subreddit(subreddit).hot(limit=5):
+            self._reply_to_post(submission)
+            self._check_comment_tree(submission)
+        self._update_replied_to()
+
+
+if __name__ == "__main__":
+    hello_bot = HelloBot()
+    hello_bot.say_hello('neeerp')
